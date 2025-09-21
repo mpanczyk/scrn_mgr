c816e90ff79ec2a4339483cd5d4ba287c73cbce0
Michał Pańczyk <mjpanczyk@gmail.com>
2025-09-21 21:44:14 +0200

    Refactor + add comments

--- scrn_mgr.ino
+++ scrn_mgr.ino
@@ -2,36 +2,29 @@
 #include <AsyncMqttClient.h>
 #include "Adafruit_VL53L0X.h"
 
-
-
-#define MQTT_PUB_DISTANCE "sensor/screen/distance"
-#define MQTT_PUB_TOUCH "sensor/screen/touch"
-
 #define LED_PIN D5
-#define SECOND 1000
+#define SECOND 1000 // 1000 milliseconds
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-unsigned int distance_threshold;
-unsigned int near_counter; // count how many times an object was near enough
-unsigned long near_applied;
-unsigned long last_up;
+unsigned int distance_threshold = 300; // mm
+unsigned long proximity_reported = 0;
+unsigned long proximity_started;
 unsigned int INF_DISTANCE = INT_MAX;
 bool state_on = false;
 
+unsigned int on_time_threshold = SECOND;
+unsigned int off_time_threshold = 10*SECOND;
+
 void setup() {
-    delay(2000);
     Serial.begin(115200);
-    Serial.println("HELLO. setup()...");
+
     pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
 
     if (!lox.begin()) {
         Serial.println("Failed to boot VL53L0X");
         while(1);
     }
-    digitalWrite(LED_PIN, LOW);
-    distance_threshold = 300;
-    near_counter = 0;
-    near_applied = 0;
 }
 
 unsigned int get_distance(){
@@ -55,13 +48,19 @@ void loop() {
     unsigned int distance = get_distance();
     unsigned long current_millis = millis();
     if(distance < distance_threshold){
-        if (current_millis > last_up + SECOND){ // stay for one second to turn on
-            near_applied = current_millis;
+        // Wait for `on_time_threshold` seconds before reporting proximity.
+        if (current_millis > proximity_started + on_time_threshold){
+            proximity_reported = current_millis;
             change(true);
         }
     } else {
-        last_up = current_millis;
-        if(current_millis > near_applied + 10*SECOND){
+        // `proximity_started` records the last time an object was far away.
+        // When this no longer holds, it means beginning of an object is close.
+        proximity_started = current_millis;
+
+        // If there was no proximity for `off_time_threshold` seconds since `proximity_reported`,
+        // we report the end of proximity.
+        if(current_millis > proximity_reported + off_time_threshold){
             change(false);
         }
     }
